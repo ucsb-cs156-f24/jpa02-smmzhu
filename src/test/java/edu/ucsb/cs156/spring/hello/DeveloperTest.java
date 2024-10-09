087c64ebcc419c1cca9e20b31f3e52720a24fcdd
smmzhu <smmzhu@gmail.com>
2024-10-08 18:03:21 -0700

    sz - updated with actual github id

--- src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
@@ -26,6 +26,11 @@ public class DeveloperTest {
         assertEquals("Samuel Z.", Developer.getName());
     }
 
+    @Test
+    public void getGithubId_returns_correct_githubId() {
+        assertEquals("smmzhu", Developer.getGithubId());
+    }
+
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
 
